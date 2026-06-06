6230891bcef95c538877bd871a1a28ad07e0ce59
Taehoon Kwon <taevel02@gmail.com>
2026-06-06 15:16:02 +1000

    chore: update font-yeomil-mono URLs and homepage

--- Casks/font-yeomil-mono.rb
+++ Casks/font-yeomil-mono.rb
@@ -2,10 +2,10 @@ cask "font-yeomil-mono" do
   version "1.0.0"
   sha256 :no_check
 
-  url "https://github.com/taevel02/yeomil-mono/releases/download/v#{version}/YeomilMono-TTF.zip"
+  url "https://github.com/buddy-proiectio/yeomil-mono/releases/download/v#{version}/YeomilMono-TTF.zip"
   name "Yeomil Mono"
   desc "Unified monospace font merging Geist Mono and Pretendard CJK for developer environments"
-  homepage "https://github.com/taevel02/yeomil-mono"
+  homepage "https://github.com/buddy-proiectio/yeomil-mono"
 
   font "YeomilMono-Regular.ttf"
   font "YeomilMono-Bold.ttf"
